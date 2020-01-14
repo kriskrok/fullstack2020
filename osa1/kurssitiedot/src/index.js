3059380e872f28fb358683136fd845431dec1252
Kristian <25787775+kriskrok@users.noreply.github.com>
2020-01-14 09:50:28 +0200

    Adds 1.2: kurssitiedot, step2

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -9,13 +9,19 @@ const Header = (props) => (
 
 const Content = (props) => {
   return (
-    <React.Fragment>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
-    </React.Fragment>
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
   )
 }
+
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
+)
 
 const Total = (props) => {
   return (
