e2080f22d9f9ca1c11167d2b26f05fb366d4a5b8
kriskrok <25787775+kriskrok@users.noreply.github.com>
2020-01-26 18:53:27 +0200

    Adds 1.14: anekdootit step3

--- week1/anekdootit/src/index.js
+++ week1/anekdootit/src/index.js
@@ -1,23 +1,41 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+const Title = ({ title }) => (<h2>{title}</h2>)
 
 const Button = ({ handleClick, text }) => (<button onClick={handleClick}>{text}</button>)
 
 const getRandomInt = (min, max) => (Math.floor(Math.random() * (max - min) + min))
 
-const App = (props) => {
+const Anecdote = ({ text, votes }) => (<>{text}<br />has {votes} votes</>)
+
+const App = ({ anecdotes }) => {
   const [selected, setSelected] = useState(0);
+  const [score, setScore] = useState(new Array(anecdotes.length).fill(0))
 
   const handleClick = () => {
     setSelected(getRandomInt(0, anecdotes.length))
   }
+
+  const highest = score.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+
+   const handleVoteClick= () => {
+    const copy = Array.from(score);
+    copy[selected] += 1
+    setScore(copy)
+  }
     
   return (
-    <div>
-      {props.anecdotes[selected]}<br />
+    <>
+      <Title title='Anecdote of the day' />
+      {anecdotes[selected]}<br />has {score[selected]} votes<br />
+
+      <Button handleClick={handleVoteClick} text='vote' />
       <Button handleClick={handleClick} text='next anecdote' />
-    </div>
+
+      <Title title='Anecdote with most votes' />
+      <Anecdote text={anecdotes[score.indexOf(Math.max(...score))]} votes={score[highest]} />
+    </>
   )
 }
 
@@ -31,4 +49,3 @@ const anecdotes = [
 ]
 
 ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
-
