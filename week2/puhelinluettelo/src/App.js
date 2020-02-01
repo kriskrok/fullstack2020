5344dadfc87c66219621e9f03d2e075b76fc9b40
kriskrok <25787775+kriskrok@users.noreply.github.com>
2020-02-02 01:07:40 +0200

    Adds 2.8: puhelinluettelo step3

--- week2/puhelinluettelo/src/App.js
+++ week2/puhelinluettelo/src/App.js
@@ -2,33 +2,43 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
-  const [ newName, setNewName ] = useState('')
+  const [ newPerson, setNewPerson ] = useState(
+    { name: '', number: '' }
+  )
 
-  const previouslyAdded = persons.some(person => person.name === newName)
+  const previouslyAdded = persons.some(person => person.name === newPerson.name)
 
-  const addNumber = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
 
     if (previouslyAdded) {
-      alert(`${newName} is already added to phonebook`)
+      alert(`${newPerson.name} is already added to phonebook`)
     } else {
       const personObject = {
-        name: newName
+        name: newPerson.name,
+        number: newPerson.number
       }  
       setPersons(persons.concat(personObject))
     }
 
-    setNewName('')
+    setNewPerson({ ...newPerson, name: '', number: ''})
+  }
+
+  const handleNameChange = (event) => {
+    setNewPerson({ ...newPerson, name: event.target.value})
   }
 
   const handleNumberChange = (event) => {
-    setNewName(event.target.value)
+    setNewPerson({ ...newPerson, number: event.target.value})
   }
 
   const Contact = ({ contact }) => (
-    <li>{contact.name}</li>
+    <li>{contact.name} {contact.number}</li>
   )
 
   return (
@@ -36,9 +46,12 @@ const App = () => {
     <React.Fragment>
       <h2>Phonebook</h2>
 
-      <form onSubmit={addNumber}>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNumberChange} />
+          name: <input value={newPerson.name} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newPerson.number} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -53,7 +66,6 @@ const App = () => {
       </ul>
     </React.Fragment>
   )
-
 }
 
 export default App;
