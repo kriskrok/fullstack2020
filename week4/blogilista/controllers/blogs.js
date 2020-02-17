bd8f5f87bc575185aa36464af2a9e3ea1bc3b2df
kriskrok <25787775+kriskrok@users.noreply.github.com>
2020-02-17 19:05:55 +0200

    Adds 4.14 blogilistan laajennus, step2

--- week4/blogilista/controllers/blogs.js
+++ week4/blogilista/controllers/blogs.js
@@ -7,21 +7,45 @@ blogsRouter.get('/', (req, res) => {
   })
 })
 
-blogsRouter.post('/', (req, res, next) => {
+blogsRouter.post('/', async (req, res, next) => {
   const body = req.body
 
-  const blog = new Blog({
+  if (!!body.title && !!body.url) {
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes === undefined ? 0 : Number(body.likes)
+    })
+
+    const savedBlog = await blog.save()
+    res.status(201).json(savedBlog.toJSON())
+  /*   blog.save()
+      .then(savedBlog => {
+        res.status(201).json(savedBlog.toJSON())
+      })
+      .catch(error => next(error))*/
+  } else {
+    res.status(400).end()
+  } 
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+  const body = req.body
+  const blog = {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes === undefined ? 0 : Number(body.likes)
-  })
+    likes: body.likes
+  }
 
-  blog.save()
-    .then(savedBlog => {
-      res.status(201).json(savedBlog.toJSON())
-    })
-    .catch(error => next(error))
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+  res.json(updatedBlog.toJSON())
+})
+
+blogsRouter.delete('/:id', async (req, res) => {
+  await Blog.findByIdAndRemove(req.params.id)
+  res.status(204).end()
 })
 
 module.exports = blogsRouter
--- week4/blogilista/tests/blog_api.test.js
+++ week4/blogilista/tests/blog_api.test.js
@@ -6,85 +6,159 @@ const api = supertest(app)
 
 const Blog = require('../models/blog')
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(helper.initialBlogs)
+describe('when there is initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    const response = await Blog.insertMany(helper.initialBlogs)
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain('Canonical string reduction')
+  })
+
+  test('blogs have an ID', async () => {
+    const response = await api.get('/api/blogs')
+    let blog = response.body[0]
+
+    expect(blog).toHaveProperty('id')
+    expect(blog._id).toBeUndefined()
+  })
+
+
+  describe('addition of a new blog', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = {
+        title: 'The Winnie the Pooh Guide to Blogging',
+        author: 'James Chartrand',
+        url: 'https://copyblogger.com/winnie-the-pooh-blogging/',
+        likes: 9001
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+      const titles = blogsAtEnd.map(b => b.title)
+      expect(titles).toContain('The Winnie the Pooh Guide to Blogging')
+    })
+
+    test('without likes gets assigned 0 likes', async () => {
+      const newBlog = {
+        title: 'The Winnie the Pooh Guide to Blogging',
+        author: 'James Chartrand',
+        url: 'https://copyblogger.com/winnie-the-pooh-blogging/'
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      let postedBlog = blogsAtEnd.find(blog =>
+        blog.title === 'The Winnie the Pooh Guide to Blogging' &&
+        blog.author === 'James Chartrand' &&
+        blog.url === 'https://copyblogger.com/winnie-the-pooh-blogging/'
+      )
+
+      expect(postedBlog).toHaveProperty('likes')
+      expect(postedBlog.likes).toBe(0)
+    })
+
+    test('fails with status code 400 if title is missing', async () => {
+      const newBlog = {
+        author: 'James Chartrand',
+        url: 'https://copyblogger.com/winnie-the-pooh-blogging/'
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
+    })
+
+    test('fails with status code 400 if url is missing', async () => {
+      const newBlog = {
+        title: 'The Winnie the Pooh Guide to Blogging',
+        author: 'James Chartrand'
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
+
+  })
+
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      const contents = blogsAtEnd.map(r => r.url)
+
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+      expect(contents).not.toContain(blogToDelete.url)
+    })
+  })
+
+  describe('update of a blog', () => {
+    test('succeeds with a status code 200 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogAtStart = blogsAtStart[1]
+      const blogToUpdate = {
+        ...blogAtStart,
+        likes: 12345
+      }
+
+      const response = await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(blogToUpdate)
+        .expect(200)
+
+      const updatedBlog = response.body
+
+      expect(updatedBlog.likes).not.toBe(blogAtStart.likes)
+      expect(updatedBlog.likes).toBe(12345)
+    })
+  })
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
-
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
-
-  const titles = response.body.map(r => r.title)
-  expect(titles).toContain('Canonical string reduction')
-})
-
-test('blogs have an ID', async () => {
-  const response = await api.get('/api/blogs')
-  let blog = response.body[0]
-
-  expect(blog).toHaveProperty('id')
-  expect(blog._id).toBeUndefined()
-})
-
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'The Winnie the Pooh Guide to Blogging',
-    author: 'James Chartrand',
-    url: 'https://copyblogger.com/winnie-the-pooh-blogging/',
-    likes: 9001
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length +1)
-
-  const titles = blogsAtEnd.map(b => b.title)
-  expect(titles).toContain('The Winnie the Pooh Guide to Blogging')
-})
-
-test('blog without likes gets assigned 0 likes', async () => {
-  const newBlog = {
-    title: 'The Winnie the Pooh Guide to Blogging',
-    author: 'James Chartrand',
-    url: 'https://copyblogger.com/winnie-the-pooh-blogging/'
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  let postedBlog = blogsAtEnd.filter(blog => 
-    blog.title === 'The Winnie the Pooh Guide to Blogging' &&
-    blog.author === 'James Chartrand' &&
-    blog.url === 'https://copyblogger.com/winnie-the-pooh-blogging/'
-  )
-
-  expect(...postedBlog).toHaveProperty('likes')
-  expect(postedBlog[0].likes).toBe(0)
-})
-
-afterAll (() => {
+afterAll(() => {
   mongoose.connection.close()
 })
