6bbfb5adad92d6effd4d553c7a0cdc4cc86150f3
kriskrok <25787775+kriskrok@users.noreply.github.com>
2020-02-18 17:00:11 +0200

    Adds 4.16: blogilistan laajennus, step5

---
+++ week4/blogilista/controllers/users.js
@@ -0,0 +1,40 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User
+    .find({}).populate('blogs', { content: 1, date: 1 })
+  response.json(users.map(u => u.toJSON()))
+})
+
+usersRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  if (!body.password || body.password.length < 3) {
+    const message = !body.password
+      ? 'missing password field, thou shall provide a password'
+      : 'invalid password, password has to be atleast 3 characters long'
+
+    return response.status(400).json({
+      error: `${message}`
+    }).end()
+  }
+
+  const saltRounds = 15
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  console.log('mansikkaraparperi', savedUser)
+
+  response.json(savedUser)
+})
+
+module.exports = usersRouter
--- week4/blogilista/tests/blog_api.test.js
+++ week4/blogilista/tests/blog_api.test.js
@@ -5,12 +5,24 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 describe('when there is initially some blogs saved', () => {
   beforeEach(async () => {
     await Blog.deleteMany({})
     const response = await Blog.insertMany(helper.initialBlogs)
   })
+
+  /*beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = helper.initialBlogs
+      .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+
+    const valmis = await Promise.all(promiseArray)
+    valmis.map(blog => blog.toJSON())
+  })*/
 
   test('blogs are returned as json', async () => {
     await api
@@ -157,6 +169,144 @@ describe('when there is initially some blogs saved', () => {
       expect(updatedBlog.likes).toBe(12345)
     })
   })
+
+  describe('when there is initially one user at db', () => {
+    beforeEach(async () => {
+      await User.deleteMany({})
+      const user = new User({
+        username: 'root',
+        name: 'Remontti Reiska',
+        password: 'sekret'
+      })
+      await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'mehi',
+        name: 'Maija Mehiläinen',
+        password: 'salasana',
+      }
+
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
+
+      const usernames = usersAtEnd.map(u => u.username)
+      expect(usernames).toContain(newUser.username)
+    })
+
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'root',
+        name: 'Superuser',
+        password: 'salainen',
+      }
+
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      expect(result.body.error).toContain('`username` to be unique')
+
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd.length).toBe(usersAtStart.length)
+    })
+
+    test('creation fails with proper statuscode and message if username is shorter than 3 characters', async () => {      
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'rö',
+        name: 'Röllimetsän Rölli',
+        password: 'likaiset_varpaat',
+      }
+
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      expect(result.body.error).toContain(`\`username\` (\`${newUser.username}\`) is shorter than the minimum`)
+
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
+
+    test('creation fails with proper statuscode and message if username is missing', async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        name: 'Vappu Satanen',
+        password: 'harmaa_talous',
+      }
+
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      expect(result.body.error).toContain('`username` is required.')
+
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
+
+    test('creation fails with proper statuscode and message if password is shorter than 3 characters', async () => {      
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'Rölli',
+        name: 'Röllimetsän Rölli',
+        password: 'lv',
+      }
+
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      expect(result.body.error).toContain('invalid password, password has to be atleast 3 characters long')
+
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
+
+    test('creation fails with proper statuscode and message if password is missing', async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'vstanen',
+        name: 'Vappu Satanen',
+      }
+
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      expect(result.body.error).toContain('missing password field, thou shall provide a password')
+
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
+  })
+
 })
 
 afterAll(() => {
