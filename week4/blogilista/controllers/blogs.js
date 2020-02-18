426debe50af3ea1701a0dca1f1e5e68427590ceb
kriskrok <25787775+kriskrok@users.noreply.github.com>
2020-02-18 19:42:37 +0200

    Adds 4.19: blogilistan laajennus, step8

--- week4/blogilista/controllers/blogs.js
+++ week4/blogilista/controllers/blogs.js
@@ -1,20 +1,33 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog
-    .find({}).populate('user', { blogs: 0 }) //username, name, id
-
+    .find({}).populate('user', { blogs: 0 })
   res.json(blogs.map(blog => blog.toJSON()))
-
-
 })
 
 blogsRouter.post('/', async (req, res, next) => {
   const body = req.body
+  const token = getTokenFrom(req)
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  console.log('käyttäjäMME: ', user)
 
   if (!!body.title && !!body.url) {
     const blog = new Blog({
