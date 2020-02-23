1f6c9ef62580eb358fd2891ffc2680a2a00660f4
kriskrok <25787775+kriskrok@users.noreply.github.com>
2020-02-23 11:50:59 +0200

    Adds 5.8: blogilistan frontend, step8

--- week5/bloglist-frontend/src/components/Blog.js
+++ week5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Blog = ({ blog }) => (
-  <p>{blog.title} by <i>{blog.author}</i></p>
-)
+const Blog = ({ blog }) => {
+  const [displayAll, setdisplayAll] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleShowAll = () => setdisplayAll(!displayAll)
+
+  if (displayAll) {
+    return (
+      <div style={blogStyle}>
+      {blog.title} by <i>{blog.author}</i><button onClick={() => toggleShowAll()}>close</button><br />
+      {blog.url}<br />
+      likes {blog.likes} <button>like</button><br />
+      {blog.user.name}
+    </div>
+    )
+  }
+  return (
+    <div style={blogStyle}>
+      {blog.title} by <i>{blog.author}</i><button onClick={() => toggleShowAll()}>view</button>
+    </div>
+  )
+}
 
 export default Blog
