bfaa03a5726840dc40460c20c68672fe69429c36
kriskrok <25787775+kriskrok@users.noreply.github.com>
2020-02-24 13:06:29 +0200

    Adds 5.15: blogilistan testit, step2

--- week5/bloglist-frontend/src/components/Blog.test.js
+++ week5/bloglist-frontend/src/components/Blog.test.js
@@ -1,39 +1,80 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('to be named', () => {
-  const blog = {
+describe('<Blog />' , () => {
+  let component
+  let blog = {
     'title': 'The Winnie the Pooh Guide to Bloggin',
     'author': 'James Charthand',
     'url': 'https://copyblogger.com/winnie-the-pooh-blogging',
-    'likes': 9001
+    'likes': 9001,
+    'user': {
+      'username': 'Maija',
+      'id': '5e4be0a737acfc138c0ad002',
+      'name': 'Maija Mehiläinen',
+    },
   }
 
-  const component = render(
-    <Blog blog={blog} />
-  )
+  const likeBlog = jest.fn()
 
-  expect(component).toBeDefined()
+  beforeEach(() => {
+    component = render(
+      <Blog blog={blog} updateBlog={likeBlog}/>
+    )
+  })
 
-  const hideButton = component.container.querySelector('.hide-blog-button')
-  const expandButton = component.container.querySelector('.expand-blog-button')
-  const element = component.getByText('James Charthand')
+  test('at start only blog title and author are displayed', () => {
+    expect(component).toBeDefined()
+    expect(component.displayAll).toBeFalsy()
 
-  expect(component.container).not.toContainElement(hideButton)
-  expect(component.container).toContainElement(expandButton)
-  expect(element).toBeDefined()
+    const hideButton = component.container.querySelector('.hide-blog-button')
+    expect(component.container).not.toContainElement(hideButton)
 
-  expect(component.container).not.toHaveTextContent(
-    'copyblogger.com/winnie-the-pooh-blogging'
-  )
+    const expandButton = component.container.querySelector('.expand-blog-button')
+    expect(component.container).toContainElement(expandButton)
 
-  expect(component.container).not.toHaveTextContent(
-    'likes 9001'
-  )
+    const element = component.getByText('James Charthand')
+    expect(element).toBeDefined()
 
-  expect(component.container).toHaveTextContent(
-    'The Winnie the Pooh Guide to Bloggin'
-  )
+    expect(component.container).toHaveTextContent(
+      'The Winnie the Pooh Guide to Bloggin'
+    )
+  })
+
+  test('at start url and likes are not displayed', () => {
+    expect(component.container).not.toHaveTextContent(
+      'copyblogger.com/winnie-the-pooh-blogging'
+    )
+    expect(component.container).not.toHaveTextContent(
+      'likes 9001'
+    )
+  })
+
+  describe('after clicking view button', () => {
+    beforeEach(() => {
+      const expandButton = component.container.querySelector('.expand-blog-button')
+      fireEvent.click(expandButton)
+    })
+
+    test('url and likes are displayed', () => {
+      expect(component.container).toHaveTextContent(
+        'https://copyblogger.com/winnie-the-pooh-blogging'
+      )
+      expect(component.container).toHaveTextContent(
+        'likes 9001'
+      )
+    })
+
+    test('pressing like-button twice invokes the eventHandler twice', () => {
+      const likeButton = component.getByText('like')
+      expect(likeButton).toBeDefined()
+
+      fireEvent.click(likeButton)
+      fireEvent.click(likeButton)
+
+      expect(likeBlog.mock.calls.length).toBe(2)
+    })
+  })
 })
